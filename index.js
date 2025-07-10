ec00b7bbcc9b9c6db323922536ea14e0bac61c11
Sam Willard <144750636+willarsa@users.noreply.github.com>
2025-07-09 22:04:36 -0700

    added more stuff, and load pages

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function blank(){
-    
+function loadPage(tab){
+    var pages = document.getElementsByClassName('page');
+
+    for(let i = 0; i < pages.length; i++){
+        if(pages[i].id == tab.name){
+            pages[i].classList.remove('hidden');
+        }
+        else{
+            pages[i].classList.add('hidden');
+        }
+    }
 }
