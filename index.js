8a16af8fefd03a367b1ecf82ada40416b9b741cd
Sam Willard <144750636+willarsa@users.noreply.github.com>
2025-07-11 18:58:20 -0700

    added clock

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ function loadPage(tab){
         }
     }
 }
+
+let countdown;
+let startStop = false;
+function startTimer() {
+    startStop = !startStop;
+    clearInterval(countdown); // reset if already running
+
+    let timeLeft = 60;
+    document.getElementById("timer").textContent = timeLeft;
+
+    if(startStop){
+        countdown = setInterval(function () {
+        timeLeft--;
+        document.getElementById("timer").textContent = timeLeft;
+
+        if (timeLeft <= 0) {
+            clearInterval(countdown);
+            document.getElementById("timer").textContent = "Time's up!";
+        }
+        }, 1000); 
+    }
+    
+}
